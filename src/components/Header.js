fef7e28eff2da9118810b252e0412968c8c8ffd4
danton03 <37675973+danton03@users.noreply.github.com>
2022-05-21 23:52:19 -0300

    :memo: Feat: Voltar para a Home clicando no topo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,11 @@
-import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import { Topo } from '../styles/HeaderStyles';
+
 export default function Header() {
   return(
-    <Topo>CINEFLEX</Topo>
+    <Topo>
+      <Link to={"/"}>CINEFLEX</Link>
+    </Topo>
   );
 }
 
-const Topo = styled.header`
-  position: fixed;
-  top: 0;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  max-width: 1366px;
-  height: 67px;
-
-  font-weight: 500;
-  font-size: 34px;
-  line-height: 40px;
-  background-color: #002069;
-  color:#FFFFFF;
-`;
---
+++ src/styles/HeaderStyles.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+const Topo = styled.header`
+  position: fixed;
+  top: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  max-width: 1366px;
+  height: 67px;
+
+  font-weight: 500;
+  font-size: 34px;
+  line-height: 40px;
+  background-color: #002069;
+  color:#FFFFFF;
+`;
+
+export {Topo}
